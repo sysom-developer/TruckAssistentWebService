eaf1f782028175bf73c1cfd48aec68d41060e123
andy0010 <13427591746@163.com>
2015-11-22 19:49:09 +0800

    添加redis配置

--- obd_server/config/redis.php
+++ obd_server/config/redis.php
@@ -2,7 +2,10 @@
 
 return [
 
-    'host' => '127.0.0.1',
+//    'host' => '127.0.0.1',
+    'host' => 'd342ed022afb42df.m.cnhza.kvstore.aliyuncs.com',
     'port' => 6379,
+    'instanceID' => 'd342ed022afb42df',
+    'password' => 'Sysom123'
 
 ];
