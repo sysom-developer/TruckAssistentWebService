d5a735aa08250773dd3ba2966a5310762d49ac46
andy0010 <13427591746@163.com>
2015-12-24 22:02:30 +0800

    修复修改密码

--- app_server/application/controllers/public_app_driver/Device.php
+++ app_server/application/controllers/public_app_driver/Device.php
@@ -31,6 +31,28 @@ class Device extends Public_Android_Controller {
     }
 
     public function relieve_device(){
+        $obd_device_no = trim($this->input->get_post('obd_device_no', TRUE));
+
+        if (empty($this->data['driver_id'])) {
+            $this->app_error_func(1399, 'driver_id 参数错误');
+            exit;
+        }
+
+        if (empty($obd_device_no)) {
+            $this->app_error_func(6666, '输入obd设备号');
+            exit;
+        }
+
+        $where = [
+            'driver_id' => $this->data['driver_id']
+        ];
+
+        $data = [
+            'obd_device_no' => $obd_device_no,
+        ];
+
+        $this->common_model->update('obd_device', $data, $where);
+
         echo json_en($this->data['error']);
         exit;
     }
